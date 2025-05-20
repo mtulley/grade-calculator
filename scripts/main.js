da20e5e6b45e301d0af34b7ed67382c0ba9e1de9
mtulley <mftulley@gmail.com>
2025-05-19 20:35:23 -0700

    Updated header banner
    Clicking a nav button which is already clicked does nothing. Changed border properties.

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,38 @@
 let nav_button_1 = document.querySelector(".nav-button-1")
 nav_button_1.addEventListener("click", navButton1Listener)
 
-function navButton1Listener() {
-    nav_button_1.classList.toggle("clicked")
-    nav_button_2.classList.add("nav-button-2")
-    nav_button_3.classList.add("nav-button-3")
-    nav_button_2.classList.remove("clicked")
-    nav_button_3.classList.remove("clicked")
-}
-
 let nav_button_2 = document.querySelector(".nav-button-2")
 nav_button_2.addEventListener("click", navButton2Listener)
-
-function navButton2Listener() {
-    nav_button_2.classList.toggle("clicked")
-    nav_button_1.classList.add("nav-button-1")
-    nav_button_3.classList.add("nav-button-3")
-    nav_button_1.classList.remove("clicked")
-    nav_button_3.classList.remove("clicked")
-}
 
 let nav_button_3 = document.querySelector(".nav-button-3")
 nav_button_3.addEventListener("click", navButton3Listener)
 
+function navButton1Listener() {
+    if (!nav_button_1.classList.contains("clicked")) {
+        nav_button_1.classList.toggle("clicked")
+        nav_button_2.classList.add("nav-button-2")
+        nav_button_3.classList.add("nav-button-3")
+        nav_button_2.classList.remove("clicked")
+        nav_button_3.classList.remove("clicked")
+    }
+}
+
+function navButton2Listener() {
+    if (!nav_button_2.classList.contains("clicked")) {
+        nav_button_2.classList.toggle("clicked")
+        nav_button_1.classList.add("nav-button-1")
+        nav_button_3.classList.add("nav-button-3")
+        nav_button_1.classList.remove("clicked")
+        nav_button_3.classList.remove("clicked")
+    }
+}
+
 function navButton3Listener() {
-    nav_button_3.classList.toggle("clicked")
-    nav_button_1.classList.add("nav-button-1")
-    nav_button_2.classList.add("nav-button-2")
-    nav_button_1.classList.remove("clicked")
-    nav_button_2.classList.remove("clicked")
+    if (!nav_button_3.classList.contains("clicked")) {
+        nav_button_3.classList.toggle("clicked")
+        nav_button_1.classList.add("nav-button-1")
+        nav_button_2.classList.add("nav-button-2")
+        nav_button_1.classList.remove("clicked")
+        nav_button_2.classList.remove("clicked")
+    }
 }
